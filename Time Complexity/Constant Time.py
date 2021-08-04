8489c911a73a8c3609df9fead9240f4c99985666
Laxmi Kanta Dahal <dahalp30@gmail.com>
2021-08-04 19:12:53 +0545

    Update Constant Time.py

--- Time Complexity/Constant Time.py
+++ Time Complexity/Constant Time.py
@@ -1,5 +1,7 @@
-#constant time complexity example
-if a>b:
-  return True
-else:
-  return False
+#Example for time complexiy at constant time
+def great(a,b):
+    if a>b:
+       return True
+    else:
+        return False
+print(great(4,5))
